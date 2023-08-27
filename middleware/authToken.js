e953647dbbbf45667f3f47dce03aba61fa0f9842
Ishan Singh <136164702+ishansingh1010@users.noreply.github.com>
2023-08-27 22:48:34 +0530

    addAuthenticationLogger

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -1,24 +1,29 @@
 const jwt = require('jsonwebtoken')
 
+const tokenLogger = require('../utils/tokenLogger/tokenLogger')
+
 const userAuthentication = async (req, res, next) => {
     const authHeader = req.headers.Authorization || req.headers.authorization;
     if (authHeader && authHeader.startsWith('Bearer')) {
         let token = authHeader.split(' ')[1];
         jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
             if (err) {
+                tokenLogger.log('error', "User authentication failed!")
                 res.status(401).json({
                     success: false,
-                    message: "User authentication failed"
+                    message: "User authentication failed!"
                 });
             } else {
+                tokenLogger.log('info', "User authentication pass!")
                 req.user = decoded.userData;
                 next();
             }
         })
     } else {
+        tokenLogger.log('error', "Token not found!")
         res.status(401).json({
             success: false,
-            message: "Token Not Found"
+            message: "Token not found!"
         })
     }
 }
