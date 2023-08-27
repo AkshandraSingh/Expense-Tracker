200fce5e89abde609953b70e1761a6724705be1d
Ishan Singh <136164702+ishansingh1010@users.noreply.github.com>
2023-08-27 22:40:41 +0530

    addAuthentication

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 
 const category = require('../controllers/categoryController')
 const categoryValidations = require('../validations/category/categoryValidatorSchema')
+const { userAuthentication } = require('../middleware/authToken')
 
 const router = express.Router()
 
-router.post('/createCategory/:userId', categoryValidations.createCategory, category.createCategory)
+router.post('/createCategory/:userId', userAuthentication, categoryValidations.createCategory, category.createCategory)
 
 module.exports = router
--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -2,14 +2,15 @@ const express = require('express')
 
 const expense = require('../controllers/expenseController')
 const expenseValidation = require('../validations/expense/expenseValidator')
+const { userAuthentication } = require('../middleware/authToken')
 
 const router = express.Router()
 
-router.post('/createExpense/:userId', expenseValidation.createExpense, expense.createExpense)
-router.patch('/editExpense/:expenseId', expense.editExpense)
-router.delete('/deleteExpense/:expenseId', expense.deleteExpense)
-router.get('/searchByName/:userId', expense.searchByName)
-router.get('/searchByCategory/:userId', expense.searchByCategory)
-router.get('/searchByDate/:userId', expense.searchByDate)
+router.post('/createExpense/:userId', userAuthentication, expenseValidation.createExpense, expense.createExpense)
+router.patch('/editExpense/:expenseId', userAuthentication, expense.editExpense)
+router.delete('/deleteExpense/:expenseId', userAuthentication, expense.deleteExpense)
+router.get('/searchByName/:userId', userAuthentication, expense.searchByName)
+router.get('/searchByCategory/:userId', userAuthentication, expense.searchByCategory)
+router.get('/searchByDate/:userId', userAuthentication, expense.searchByDate)
 
 module.exports = router
